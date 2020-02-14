12506a85dbcb90ec9da18d6c3b8dcdb153fff6e8
norio c <norionorioc@gmail.com>
2020-02-14 20:04:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,7 +9,10 @@ end
 end
 
 def sort_array_char_count 
-  
+  array.sort do |left, right| 
+    right.length <=> left.length
+end
+end
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
