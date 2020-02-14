3e312d722c8e261d06166a4a6cf34c6c8d745eb8
norio c <norionorioc@gmail.com>
2020-02-14 20:02:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |left, right| 
+    right <=> left 
+end
+end
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+end
+  
+  
