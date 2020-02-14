37773bccc8f97044cff6038c25211d22236e5c48
norio c <norionorioc@gmail.com>
2020-02-14 20:16:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,10 +40,12 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index 
-
-
-
-
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
 
