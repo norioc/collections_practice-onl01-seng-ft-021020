c755dc8e38cbfdafa517a67f603de645eeaa5344
norio c <norionorioc@gmail.com>
2020-02-14 20:16:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,9 +41,14 @@ end
 
 def add_s(array)
   [1,2].each_with_index.collect{|element, index| }
-
-
-
-
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
+
 
